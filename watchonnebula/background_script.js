62a94ea8fb86fc777bfe1aa8ff88453198472377
Parker Addison <pargrey@gmail.com>
2021-01-09 16:50:23 -0800

    Core functionality working. Turns out you need to wait for 0ms to get javascript to work right ((((:

--- watchonnebula/background_script.js
+++ watchonnebula/background_script.js
@@ -1,6 +1,105 @@
 // Put all the javascript code here, that you want to execute in background.
 
-// Get the collection of Nebula creators and their YouTube links.
+// Get all of the creators that are on Nebula and their youtube channels.
+getNebulaCreators()
+// When a content script visits a youtube video, it'll send the channel info and
+// we'll check if the channel belongs to a Nebula creator.
+browser.runtime.onMessage.addListener(checkCreator);
+
+async function getNebulaCreators() {
+
+    console.log('GETTING CREATORS');
+
+    htmlPromise = loadHTML('https://standard.tv', parse=true);
+    htmlPromise.then(function (doc) {
+
+        let creators =  doc.querySelectorAll('.youtube-creator');
+        let creatorData = Array.from(creators).map(d => {
+            return JSON.parse(JSON.stringify(d.querySelector('a').dataset))
+        });
+
+        // for (i in creatorData) {
+
+        //     let creatorYT = creatorData[i].youtube;
+
+        //     if (!creatorYT) continue;
+
+        //     // We need to clean up the url to avoid CORS problems. Namely, let's
+        //     // make sure they all start with https://.
+        //     if (creatorYT.startsWith('http://')) {
+        //         creatorYT = 'https://' + creatorYT.slice(7,);
+        //     } else if (!creatorYT.startsWith('https://')) {
+        //         creatorYT = 'https://' + creatorYT;
+        //     }
+
+        //     var channelID;
+        //     // If the link is a /channel link then just extract the ID
+        //     if (creatorYT.includes('/channel/')) {
+        //         splitted = creatorYT.split('/');
+        //         channelID = splitted[splitted.indexOf('channel')+1];
+        //         // console.log(`${data.title} has direct channel link with ID ${channelID}`);
+        //     } else {
+        //         // If it's a user link or custom url then perform ID extraction
+        //         loadHTML(creatorYT)
+        //         .then(function (html) {
+        //             // // See: https://stackoverflow.com/questions/47096243/how-to-get-youtube-channel-id-using-channel-custom-name
+        //             // let pattern = /(?<=canonical" href="https:\/\/(?:www\.)?youtube.com\/channel\/)[^"]*/;
+
+        //             // Another way:
+        //             let pattern = /(?<="urlCanonical":")[^".]*/;
+        //             channelID = html.match(pattern);
+        //         })
+        //     }
+
+        //     creatorData[i].channelID = channelID ? channelID : false;
+        // }
+
+        // Expose to other functions
+        window.creatorData = creatorData;
+        console.log('GETTING CREATORS COMPLETE');
+    });
+}
+
+function checkCreator(message, sender) {
+
+    console.log('CHECKING YOUTUBE CHANNEL')
+
+    // Not the most elegant solution, but I was having a hard time mixing
+    // Promises with synchronous dependencies (i.e. this function relies on our
+    // getNebulaCreators being complete). But hey, this works!
+    const waitForData = setInterval(() => {
+        if (window.creatorData) {
+
+            clearInterval(waitForData);
+            
+            console.log('CREATOR DATA DEFINED');
+
+            let channelIDs = window.creatorData.map(d => d.video);
+
+            let channelMatch = channelIDs.indexOf(message.channelID);
+            console.log(message.channelID);
+            console.log(channelMatch);
+        
+            // If the creator is also on Nebula then we should get the corresponding
+            // data from Nebula and tell the content script to make an alert.
+            if (channelMatch > -1) {
+                let data = window.creatorData[channelMatch];
+        
+                console.log(data);
+        
+                // We should also try to find the link directly to the corresponding video.
+                // TODO
+        
+                browser.tabs.sendMessage(sender.tab.id, {
+                    'creatorName': data.title,
+                    'videoTitle': message.videoTitle
+                })
+            }
+        } else {
+            console.log('CREATOR DATA UNDEFINED - WAITING');
+        }
+    }, 1000);
+}
 
 async function loadHTML(url, parse=false) {
     let response = await fetch(url);
@@ -11,85 +110,3 @@ async function loadHTML(url, parse=false) {
     }
     return html;
 }
-
-async function run() {
-
-    console.log('RUNNING');
-
-    htmlPromise = loadHTML('https://standard.tv', parse=true);
-    htmlPromise.then(function (doc) {
-
-        let creators =  doc.querySelectorAll('.youtube-creator');
-        let creatorData = Array.from(creators).map(d => d.querySelector('a').dataset);
-
-
-        let channelIDs = [];
-        creatorData.forEach(function (data) {
-
-            let creatorYT = data.youtube;
-            if (!creatorYT) {
-                return;
-            }
-            // We need to clean up the url to avoid CORS problems. Namely, let's
-            // make sure they all start with https://.
-            if (creatorYT.startsWith('http://')) {
-                creatorYT = 'https://' + creatorYT.slice(7,);
-            } else if (!creatorYT.startsWith('https://')) {
-                creatorYT = 'https://' + creatorYT;
-            }
-            var channelID;
-            
-            // If the link is a /channel link then just extract the ID
-            if (creatorYT.includes('/channel/')) {
-                splitted = creatorYT.split('/');
-                channelID = splitted[splitted.indexOf('channel')+1];
-                console.log(`${data.title} has direct channel link with ID ${channelID}`);
-            }
-            
-            // If it's a user link or custom url then perform ID extraction
-            else {
-                console.log(`ATTEMPTING: ${creatorYT}`);
-                loadHTML(creatorYT)
-                .then(function (html) {
-                        // See: https://stackoverflow.com/questions/47096243/how-to-get-youtube-channel-id-using-channel-custom-name
-                        let pattern = /(?<=canonical" href="https:\/\/(?:www\.)?youtube.com\/channel\/)[^"]*/;
-                        channelID = html.match(pattern);
-                        console.log(`${data.title} has indirect link but found ID ${channelID}`)
-                    })
-            }
-
-            channelIDs.push(channelID);
-        });
-
-        window.channelIDs = channelIDs;
-
-    });
-        
-}
-
-mainRunPromise = run();
-
-async function check(message, sender) {
-    console.log(message);
-    await mainRunPromise;
-    
-    channelMatch = String(window.channelIDs.indexOf(message.channelID));
-    
-    console.log(channelMatch);
-    console.log(sender.tab.id);
-    browser.tabs.sendMessage(sender.tab.id, {channelMatch: channelMatch});
-}
-
-// Listen for a channelID from the content script, then check for a match.
-browser.runtime.onMessage.addListener(check);
-
-//     let match = creatorData.filter(d => d.youtube.replace(/\/$/,'') == youtubeChannel)
-//     if (match.length > 0) {
-//         alert(`You should be watching this on Nebula! Go to ${match[0].nebula}`)
-//     }
-
-//     let creatorChannels = creatorData.map(d => d.youtube.replace(/\/$/,''));
-//     if ( creatorChannels.includes(youtubeChannel) ) {
-
-//     }
-// })
--- watchonnebula/content_script.js
+++ watchonnebula/content_script.js
@@ -3,13 +3,57 @@
 // For handling YouTube navigation, see:
 // https://stackoverflow.com/questions/34077641/how-to-detect-page-navigation-on-youtube-and-modify-its-appearance-seamlessly/34100952#34100952
 
-window.addEventListener('yt-navigate-start', run);
+/**
+ * Basically:
+ * 
+ * - We want this to run on a normal page load w/o any YouTube nagivation
+ *   - I specified to run at document_end in the manifest, so the page should be
+ *     fully loaded when this script executes. Therefore we can just run()
+ * - We also want this to run when YouTube conducts navigation.
+ *   - If we run too soon then the video title and channel are wrong, though!
+ *   - We can add a listener for the youtube navigation perhaps...
+ *     - This works... sometimes. The progress bar doesn't always complete! We
+ *       could maybe have a fallback if a certain amount of time has passed.
+ */
+// window.addEventListener('transitionend', (event) => {
+//     if (event.target.id != 'progress') return;
+//     run();
+// });
+// const titleObserver = new MutationObserver(function callback(mutationList, observer) {
+//     run();
+// });
+// titleObserver.observe(document.querySelector('h1.ytd-video-primary-info-renderer'));
 
-if (document.body) {
-    run();
-} else {
-    document.addEventListener('DOMContentLoaded', run);
+// To support initial page load (this script runs after fully loaded)
+console.log('INITIAL LOAD');
+run();
+
+// To support forward/backward page navigation changes.
+//
+// The setTimeout must be used to ensure that this effectively runs on the *new*
+// page.
+window.onpopstate = function (event) {
+    console.log('HISTORY NAVIGATION');
+    if (event.state) {
+        setTimeout(run, 0);
+    }
 }
+
+// To support YouTube navigation events (clicking)
+//
+// Similar to history navigation, the setTimeout *must* be used, or the function
+// will essentially run on the previous page.
+window.addEventListener('yt-navigate-finish', function(event) {
+    console.log('YT NAVIGATION');
+    setTimeout(run, 0);
+});
+
+// window.addEventListener('yt-navigate-start', function() {
+//     document.addEventListener('transitionend', function(event) {
+//         if (event.target.id != 'progress') return;
+//         run();
+//     });
+// });
 
 function run() {
     if (!location.pathname.startsWith('/watch')) {
@@ -20,16 +64,26 @@ function run() {
     // background script.
     //
     // TODO: Better to use YouTube API for this.
-    let channelURL = document.querySelector('.yt-formatted-string[href^="/channel/"]').href;
-    let channelID = channelURL.split('/').pop()
-    console.log(channelID);
-    alert(channelID);
 
-    browser.runtime.sendMessage({"channelID": channelID});
+    // Easier to just serialize the page to a string and search that directly
+    // let pageString = new XMLSerializer().serializeToString(document);
+    // let channelID = pageString.match(/(?<="channelId":")[^"]*/);
+
+    let channelURL = document.querySelector(
+        '.ytd-video-owner-renderer + * .yt-formatted-string[href^="/channel/"]').href;
+    let channelID = channelURL.split('/').pop()
+    let videoTitle = document.querySelector('h1.ytd-video-primary-info-renderer').textContent;
+    console.log(`channelID: ${channelID}`);
+    console.log(`videoTitle: ${videoTitle}`);
+
+    browser.runtime.sendMessage({
+        'channelID': channelID,
+        'videoTitle': videoTitle
+    });
 }
 
 function respond(message) {
-    alert(message.channelMatch);
+    alert(`${message.creatorName} is on Nebula! Video: ${message.videoTitle}`);
 }
 
 browser.runtime.onMessage.addListener(respond);
