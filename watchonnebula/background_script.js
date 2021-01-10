be2271ad66f814b9fcba90ab62315d4f78f1ddcc
Parker Addison <pargrey@gmail.com>
2021-01-09 19:25:48 -0800

    Extension now has popup that links to nebula creator page; Clean up and polish aspects of extension.

--- watchonnebula/background_script.js
+++ watchonnebula/background_script.js
@@ -1,7 +1,7 @@
-// Put all the javascript code here, that you want to execute in background.
 
 // Get all of the creators that are on Nebula and their youtube channels.
 getNebulaCreators()
+
 // When a content script visits a youtube video, it'll send the channel info and
 // we'll check if the channel belongs to a Nebula creator.
 browser.runtime.onMessage.addListener(checkCreator);
@@ -17,42 +17,6 @@ async function getNebulaCreators() {
         let creatorData = Array.from(creators).map(d => {
             return JSON.parse(JSON.stringify(d.querySelector('a').dataset))
         });
-
-        // for (i in creatorData) {
-
-        //     let creatorYT = creatorData[i].youtube;
-
-        //     if (!creatorYT) continue;
-
-        //     // We need to clean up the url to avoid CORS problems. Namely, let's
-        //     // make sure they all start with https://.
-        //     if (creatorYT.startsWith('http://')) {
-        //         creatorYT = 'https://' + creatorYT.slice(7,);
-        //     } else if (!creatorYT.startsWith('https://')) {
-        //         creatorYT = 'https://' + creatorYT;
-        //     }
-
-        //     var channelID;
-        //     // If the link is a /channel link then just extract the ID
-        //     if (creatorYT.includes('/channel/')) {
-        //         splitted = creatorYT.split('/');
-        //         channelID = splitted[splitted.indexOf('channel')+1];
-        //         // console.log(`${data.title} has direct channel link with ID ${channelID}`);
-        //     } else {
-        //         // If it's a user link or custom url then perform ID extraction
-        //         loadHTML(creatorYT)
-        //         .then(function (html) {
-        //             // // See: https://stackoverflow.com/questions/47096243/how-to-get-youtube-channel-id-using-channel-custom-name
-        //             // let pattern = /(?<=canonical" href="https:\/\/(?:www\.)?youtube.com\/channel\/)[^"]*/;
-
-        //             // Another way:
-        //             let pattern = /(?<="urlCanonical":")[^".]*/;
-        //             channelID = html.match(pattern);
-        //         })
-        //     }
-
-        //     creatorData[i].channelID = channelID ? channelID : false;
-        // }
 
         // Expose to other functions
         window.creatorData = creatorData;
@@ -77,22 +41,23 @@ function checkCreator(message, sender) {
             let channelIDs = window.creatorData.map(d => d.video);
 
             let channelMatch = channelIDs.indexOf(message.channelID);
-            console.log(message.channelID);
-            console.log(channelMatch);
         
             // If the creator is also on Nebula then we should get the corresponding
             // data from Nebula and tell the content script to make an alert.
             if (channelMatch > -1) {
+                console.log("IT'S A MATCH!")
+                
                 let data = window.creatorData[channelMatch];
         
                 console.log(data);
         
-                // We should also try to find the link directly to the corresponding video.
-                // TODO
-        
+                // TODO: We should also try to find the link directly to the
+                // corresponding video. For now, we'll just link to the creator
+                // page.
                 browser.tabs.sendMessage(sender.tab.id, {
                     'creatorName': data.title,
-                    'videoTitle': message.videoTitle
+                    'videoTitle': message.videoTitle,
+                    'href': data.nebula
                 })
             }
         } else {
--- watchonnebula/content_script.js
+++ watchonnebula/content_script.js
@@ -1,32 +1,6 @@
-// Put all the javascript code here, that you want to execute after page load.
-
-// For handling YouTube navigation, see:
-// https://stackoverflow.com/questions/34077641/how-to-detect-page-navigation-on-youtube-and-modify-its-appearance-seamlessly/34100952#34100952
-
-/**
- * Basically:
- * 
- * - We want this to run on a normal page load w/o any YouTube nagivation
- *   - I specified to run at document_end in the manifest, so the page should be
- *     fully loaded when this script executes. Therefore we can just run()
- * - We also want this to run when YouTube conducts navigation.
- *   - If we run too soon then the video title and channel are wrong, though!
- *   - We can add a listener for the youtube navigation perhaps...
- *     - This works... sometimes. The progress bar doesn't always complete! We
- *       could maybe have a fallback if a certain amount of time has passed.
- */
-// window.addEventListener('transitionend', (event) => {
-//     if (event.target.id != 'progress') return;
-//     run();
-// });
-// const titleObserver = new MutationObserver(function callback(mutationList, observer) {
-//     run();
-// });
-// titleObserver.observe(document.querySelector('h1.ytd-video-primary-info-renderer'));
-
 // To support initial page load (this script runs after fully loaded)
 console.log('INITIAL LOAD');
-run();
+setTimeout(run, 0);
 
 // To support forward/backward page navigation changes.
 //
@@ -37,53 +11,104 @@ window.onpopstate = function (event) {
     if (event.state) {
         setTimeout(run, 0);
     }
-}
+};
 
 // To support YouTube navigation events (clicking)
 //
 // Similar to history navigation, the setTimeout *must* be used, or the function
 // will essentially run on the previous page.
-window.addEventListener('yt-navigate-finish', function(event) {
+window.addEventListener('yt-navigate-finish', function (event) {
     console.log('YT NAVIGATION');
     setTimeout(run, 0);
 });
 
-// window.addEventListener('yt-navigate-start', function() {
-//     document.addEventListener('transitionend', function(event) {
-//         if (event.target.id != 'progress') return;
-//         run();
-//     });
-// });
-
 function run() {
+
+    console.log('CONTENT SCRIPT RUN');
+
     if (!location.pathname.startsWith('/watch')) {
+        console.log('NOT A VIDEO');
         return;
     }
-    
-    // Get channel ID and video information from current page and send it to the
-    // background script.
-    //
-    // TODO: Better to use YouTube API for this.
 
-    // Easier to just serialize the page to a string and search that directly
-    // let pageString = new XMLSerializer().serializeToString(document);
-    // let channelID = pageString.match(/(?<="channelId":")[^"]*/);
+    const tryToGetElement = setInterval(() => {
+        if (document.querySelector('.ytd-video-owner-renderer')) {
+            console.log('ABLE TO QUERY VIDEO INFORMATION');
+            clearInterval(tryToGetElement);
 
-    let channelURL = document.querySelector(
-        '.ytd-video-owner-renderer + * .yt-formatted-string[href^="/channel/"]').href;
-    let channelID = channelURL.split('/').pop()
-    let videoTitle = document.querySelector('h1.ytd-video-primary-info-renderer').textContent;
-    console.log(`channelID: ${channelID}`);
-    console.log(`videoTitle: ${videoTitle}`);
-
-    browser.runtime.sendMessage({
-        'channelID': channelID,
-        'videoTitle': videoTitle
-    });
+            let channelURL = document.querySelector(
+                '.ytd-video-owner-renderer + * .yt-formatted-string[href^="/channel/"]'
+            ).href;
+            let channelID = channelURL.split('/').pop();
+        
+            let videoTitle = document.querySelector(
+                'h1.ytd-video-primary-info-renderer'
+            ).textContent;
+        
+            if (channelID && videoTitle) {
+                console.log(`channelID: ${channelID}`);
+                console.log(`videoTitle: ${videoTitle}`);
+            
+                console.log('SENDING INFORMATION');
+                browser.runtime.sendMessage({
+                    channelID: channelID,
+                    videoTitle: videoTitle,
+                });
+            } else {
+                console.log('SOME INFORMATION NOT FOUND');
+            }
+        } else {
+            console.log('WAITING FOR VIDEO INFORMATION');
+        }
+    }, 100);
 }
 
 function respond(message) {
-    alert(`${message.creatorName} is on Nebula! Video: ${message.videoTitle}`);
+    drawPopup(message.creatorName, message.videoTitle, message.href);
+}
+
+window.extensionPopup = null;
+
+function closePopup() {
+    console.log('CLOSING POPUP');
+    window.extensionPopup.style.visibility = 'hidden';
+}
+
+function drawPopup(creatorName, videoTitle, href) {
+    if (window.extensionPopup) {
+        console.log('MODIFYING POPUP');
+        window.extensionPopup.querySelector('nebext-name').innerText = creatorName;
+        window.extensionPopup.querySelector('nebext-title').innerText = videoTitle;
+        window.extensionPopup.querySelector('a').href = href;
+        window.extensionPopup.style.visibility = 'visible';
+    } else {
+        console.log('CREATING POPUP');
+        let extensionPopup = document.createElement('nebext-popup');
+        extensionPopup.innerHTML = `
+        <nebext-x>&times</nebext-x>
+        <nebext-star>
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 64 64" style="enable-background:new 0 0 64 64;" xml:space="preserve">
+        <style type="text/css">
+            .st0{fill:none;}
+            .st1{fill:#6FBCE6;}
+        </style>
+        <rect class="st0" width="64" height="64"></rect>
+        <g id="Layer_1_1_">
+            <circle class="st0" cx="32" cy="32" r="16"></circle>
+            <path class="st1" d="M26.2,30.1l-9.4,6.8h11.6L32,48l3.6-11.1h11.6l-9.4-6.8l3.6-11.1L32,25.9l-9.4-6.8L26.2,30.1z"></path>
+        </g>
+        </svg>
+        </nebext-star>
+        <h1><nebext-name>${creatorName}</nebext-name> is on Nebula!</h1>
+        <h2>You can watch <nebext-title>${videoTitle}</nebext-title> over there.</h2>
+        <a href="${href}"><button>WATCH ON NEBULA</button></a>
+        `
+        document.body.insertAdjacentElement('afterbegin', extensionPopup);
+        window.extensionPopup = extensionPopup;
+
+        // Register listener to close the popup
+        document.querySelector('nebext-x').addEventListener('click', closePopup);
+    }
 }
 
 browser.runtime.onMessage.addListener(respond);
