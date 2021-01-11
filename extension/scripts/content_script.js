e7fa7e903233b62fefca2249382fb98eccf1da42
Parker Addison <pargrey@gmail.com>
2021-01-10 16:59:18 -0800

    Remove unsafe innerHTML assignment.

--- extension/scripts/content_script.js
+++ extension/scripts/content_script.js
@@ -88,11 +88,16 @@ function drawPopup(creatorName, videoTitle, href) {
         window.extensionPopup.style.visibility = 'visible';
     } else {
         console.log('CREATING POPUP');
-        let extensionPopup = document.createElement('nebext-popup');
-        extensionPopup.classList.add('displayed');
-        extensionPopup.innerHTML = `
-        <nebext-x>&times</nebext-x>
-        <nebext-star>
+        let nebextPopup = document.createElement('nebext-popup');
+        nebextPopup.classList.add('displayed');
+
+        let nebextX = document.createElement('nebext-x');
+        nebextX.innerText = '\u00d7'; // Multiplication symbol (&times;)
+        nebextX.addEventListener('click', closePopup);
+        nebextPopup.insertAdjacentElement('beforeend', nebextX);
+
+        let nebextStar = document.createElement('nebext-star');
+        nebextStar.innerHTML = `
         <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 64 64" style="enable-background:new 0 0 64 64;" xml:space="preserve">
         <style type="text/css">
             .st0{fill:none;}
@@ -103,17 +108,32 @@ function drawPopup(creatorName, videoTitle, href) {
             <circle class="st0" cx="32" cy="32" r="16"></circle>
             <path class="st1" d="M26.2,30.1l-9.4,6.8h11.6L32,48l3.6-11.1h11.6l-9.4-6.8l3.6-11.1L32,25.9l-9.4-6.8L26.2,30.1z"></path>
         </g>
-        </svg>
-        </nebext-star>
-        <h1><nebext-name>${creatorName}</nebext-name> is on Nebula!</h1>
-        <h2>You can watch <nebext-title>${videoTitle}</nebext-title> over there to support the creator more directly!</h2>
-        <a href="${href}"><button>WATCH ON NEBULA</button></a>
-        `
-        document.body.insertAdjacentElement('afterbegin', extensionPopup);
-        window.extensionPopup = extensionPopup;
+        </svg>`
+        nebextPopup.insertAdjacentElement('beforeend', nebextStar);
 
-        // Register listener to close the popup
-        document.querySelector('nebext-x').addEventListener('click', closePopup);
+        let h1 = document.createElement('h1');
+        let nebextName = document.createElement('nebext-name');
+        nebextName.innerText = creatorName;
+        h1.insertAdjacentElement('beforeend', nebextName);
+        h1.insertAdjacentText('beforeend', ' is on Nebula!');
+        nebextPopup.insertAdjacentElement('beforeend', h1);
+        
+        let h2 = document.createElement('h2');
+        let nebextTitle = document.createElement('nebext-title');
+        nebextTitle.innerText = videoTitle;
+        h2.insertAdjacentText('beforeend', 'You can watch ');
+        h2.insertAdjacentElement('beforeend', nebextTitle);
+        h2.insertAdjacentText('beforeend', ' over there to support this creator more directly.');
+        nebextPopup.insertAdjacentElement('beforeend', h2);
+        
+        let a = document.createElement('a');
+        a.innerHTML = '<button>WATCH ON NEBULA</button>';
+        a.setAttribute('href', href);
+        nebextPopup.insertAdjacentElement('beforeend', a);
+
+        document.body.insertAdjacentElement('afterbegin', nebextPopup);
+        window.extensionPopup = nebextPopup;
+
     }
 }
 
